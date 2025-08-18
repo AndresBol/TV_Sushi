a5213f5ff9a821e4ada5bb3b724e7b5b34ba05a8
Andres <andresbol1706@gmail.com>
2025-08-18 01:26:33 -0600

    init

--- js/meal.js
+++ js/meal.js
@@ -4,39 +4,39 @@ const urlParams = new URLSearchParams(window.location.search);
 const id = parseInt(urlParams.get("id"),10);
 
 if(!id)
+{
+    console.error("No se recibió un id en la URL");
+    return;
+}
+
+const jsonURL = "https://raw.githubusercontent.com/AndresBol/TV_Sushi_Data/refs/heads/main/products.json";
+
+fetch(jsonURL)
+.then(response => response.json())
+.then(data => 
     {
-        console.error("No se recibió un id en la URL");
-        return;
-    }
+        const producto = data.find(item => item.ID === id);
 
-    const jsonURL = "https://raw.githubusercontent.com/AndresBol/TV_Sushi_Data/refs/heads/main/products.json";
-
-    fetch(jsonURL)
-    .then(response => response.json())
-    .then(data => 
-        {
-            const producto = data.find(item => item.ID === id);
-
-            if(!producto)
-                {
-                    console.error("No se encontró el producto con id:", id);
-                    return;
-                }
-
-               document.getElementById("nombreProducto").textContent = producto.Nombre;
-               document.getElementById("descripcionProducto").textContent  = producto.Descripción;
-               document.getElementById("ingredientesProducto").innerHTML= `<strong>Ingredientes:</strong> ${producto.Ingredientes}`;
-
-               document.getElementById("precioProducto").innerHTML= `<strong>Precio:</strong>${producto.Precio}`;
-               document.getElementById("porcionProducto").innerHTML= `<strong>Porcion:</strong>${producto.Porcion}`;
-
-               document.getElementById("imagenProducto").src= producto.Ruta;
-
-        })
-        .catch(error => 
+        if(!producto)
             {
-                console.error("Error al cargar los datos del producto :" , error);
-            });
+                console.error("No se encontró el producto con id:", id);
+                return;
+            }
+
+            document.getElementById("nombreProducto").textContent = producto.Nombre;
+            document.getElementById("descripcionProducto").textContent  = producto.Descripción;
+            document.getElementById("ingredientesProducto").innerHTML= `<strong>Ingredientes:</strong> ${producto.Ingredientes}`;
+
+            document.getElementById("precioProducto").innerHTML= `<strong>Precio:</strong>${producto.Precio}`;
+            document.getElementById("porcionProducto").innerHTML= `<strong>Porcion:</strong>${producto.Porcion}`;
+
+            document.getElementById("imagenProducto").src= producto.Ruta;
+
+    })
+    .catch(error => 
+        {
+            console.error("Error al cargar los datos del producto :" , error);
+        });
 
 
 });
